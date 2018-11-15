0e15127072b5b3109f183a0b7be69bb35c84f61c
jstoobz <stephensja7@gmail.com>
2018-11-15 15:48:42 -0500

    test google visions

---
+++ server/api/visions.js
@@ -0,0 +1,25 @@
+const router = require('express').Router()
+const axios = require('axios')
+module.exports = router
+
+//https://vision.googleapis.com/v1/images:annotate?key=${key}`
+
+router.post('/', async (req, res, next) => {
+  console.log(`
+
+    Inside the visions route:
+    ${req.body}
+
+  `)
+  try {
+    const { data } = await axios.post(
+      `https://vision.googleapis.com/v1/images:annotate?key=${
+        process.env.GOOGLE_VISION
+      }`,
+      req.body
+    )
+    res.json(data)
+  } catch (err) {
+    next(err)
+  }
+})
