57d021178d64a45b9171665d77cbfd596dac9a60
jstoobz <stephensja7@gmail.com>
2018-11-16 13:43:39 -0500

    routes update

--- server/index.js
+++ server/index.js
@@ -8,9 +8,51 @@ const passport = require('passport')
 const session = require('express-session')
 const SequelizeStore = require('connect-session-sequelize')(session.Store)
 const sessionStore = new SequelizeStore({ db })
+const AWS = require('aws-sdk')
+const fs = require('fs')
+const fileType = require('file-type')
+const bluebird = require('bluebird')
+const multiparty = require('multiparty')
 const app = express()
 
 if (process.env.NODE_ENV !== 'production') require('../secrets.js')
+
+AWS.config.update({
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+})
+AWS.config.setPromisesDependency(bluebird)
+const s3 = new AWS.S3()
+// abstracts function to upload a file returning a promise
+const uploadFile = (buffer, name, type) => {
+  const params = {
+    ACL: 'public-read',
+    Body: buffer,
+    Bucket: process.env.DANIK_S3_BUCKET,
+    ContentType: type.mime,
+    Key: `${name}.${type.ext}`
+  }
+  return s3.upload(params).promise()
+}
+
+// Define POST route
+app.post('/test-upload', (request, response) => {
+  const form = new multiparty.Form()
+  form.parse(request, async (error, fields, files) => {
+    if (error) throw new Error(error)
+    try {
+      const path = files.file[0].path
+      const buffer = fs.readFileSync(path)
+      const type = fileType(buffer)
+      const timestamp = Date.now().toString()
+      const fileName = `bucketFolder/${timestamp}-lg`
+      const data = await uploadFile(buffer, fileName, type)
+      return response.status(200).send(data)
+    } catch (error) {
+      return response.status(400).send(error)
+    }
+  })
+})
 
 // passport registration
 passport.serializeUser((user, done) => done(null, user.id))
