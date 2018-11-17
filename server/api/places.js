b3bb137c5022b5f2c687bd6e5772193e514ff169
Tyler Swartz <tmswartz12@gmail.com>
2018-11-17 18:42:58 -0500

    testing heroku push

--- server/api/places.js
+++ server/api/places.js
@@ -33,7 +33,6 @@ router.get('/nearby/:lat/:lng', async (req, res, next) => {
     //   ,${req.params.lng}&rankby=distance&type=restaurant&key=${
     //     process.env.GOOGLE_PLACES
     //   }`
-
     // )
     console.log('THIS IS THE DATA', data)
     res.json(data)
