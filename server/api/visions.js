3e82fe33649f72fcd411aea1662989e81db9deda
jstoobz <stephensja7@gmail.com>
2018-11-19 19:04:02 -0500

    updates for routes

--- server/api/visions.js
+++ server/api/visions.js
@@ -6,6 +6,46 @@ const visionConfig = {
   key: process.env.GOOGLE_VISION
 }
 
+const body = {
+  requests: [
+    {
+      image: {
+        // content: photo.base64
+      },
+      features: [
+        {
+          type: 'DOCUMENT_TEXT_DETECTION',
+          maxResults: 1
+        }
+      ]
+    }
+  ]
+}
+
+router.post('/', async (req, res, next) => {
+  try {
+    // const { data } = await axios.post('/')
+    console.log('inside vision post route')
+    console.log(req.body)
+    res.json('brrp')
+  } catch (err) {
+    next(err)
+  }
+})
+
+// const response = await fetch(
+//   `https://vision.googleapis.com/v1/images:annotate?key=${key}`,
+//   {
+//     method: 'POST',
+//     headers: {
+//       Accept: 'application/json',
+//       'Content-Type': 'application/json'
+//     },
+//     body: JSON.stringify(body)
+//   }
+// )
+// const parsed = await response.json()
+
 // const body = {
 //   requests: [
 //     {
@@ -22,63 +62,50 @@ const visionConfig = {
 //   ]
 // }
 
-// const response = await fetch(
-//   `https://vision.googleapis.com/v1/images:annotate?key=${key}`,
-//   {
-//     method: 'POST',
-//     headers: {
-//       Accept: 'application/json',
-//       'Content-Type': 'application/json'
-//     },
-//     body: JSON.stringify(body)
+// router.post('/', async (req, res, next) => {
+//   // Req.body: ${JSON.parse(req.body)}
+//   // Req.body.photoUri: ${JSON.parse(req.body.photoUri)}
+//   console.log(`
+
+//     Inside the visions route:
+
+//   `)
+
+//   try {
+//     const { data } = await axios.post(
+//       `https://vision.googleapis.com/v1/images:annotate?key=${
+//         process.env.GOOGLE_VISION
+//       }`,
+//       req.body.photoUri
+//     )
+
+//     console.log(`Received your POST request. You sent: ${req.body.photoUri}`)
+//     res.json(data)
+//     // res.send(`Received your POST request. You sent: ${req.body.photoUri}`)
+//   } catch (err) {
+//     next(err)
 //   }
-// )
-// const parsed = await response.json()
+//   // res.json({ brrp: 'brrrrp' })
+//   /**
+//   try {
+//     const { data } = await axios.post(      `https://vision.googleapis.com/v1/images:annotate?key=${
+//         visionConfig.key
+//       }`,
+//       {
+//         body: req.body
+//       }
+//     )
 
-router.post('/', async (req, res, next) => {
-  // Req.body: ${JSON.parse(req.body)}
-  // Req.body.photoUri: ${JSON.parse(req.body.photoUri)}
-  console.log(`
-
-    Inside the visions route:
-
-  `)
-
-  try {
-    const { data } = await axios.post(
-      `https://vision.googleapis.com/v1/images:annotate?key=${
-        process.env.GOOGLE_VISION
-      }`,
-      req.body.photoUri
-    )
-
-    console.log(`Received your POST request. You sent: ${req.body.photoUri}`)
-    res.json(data)
-    // res.send(`Received your POST request. You sent: ${req.body.photoUri}`)
-  } catch (err) {
-    next(err)
-  }
-  // res.json({ brrp: 'brrrrp' })
-  /**
-  try {
-    const { data } = await axios.post(      `https://vision.googleapis.com/v1/images:annotate?key=${
-        visionConfig.key
-      }`,
-      {
-        body: req.body
-      }
-    )
-
-    // const { data } = await axios.post(
-    //   `https://vision.googleapis.com/v1/images:annotate?key=${
-    //     process.env.GOOGLE_VISION
-    //   }`,
-    //   req.body
-    // )
-    console.log('DATA: ', data)
-    res.json(data)
-  } catch (err) {
-    next(err)
-  }
-*/
-})
+//     // const { data } = await axios.post(
+//     //   `https://vision.googleapis.com/v1/images:annotate?key=${
+//     //     process.env.GOOGLE_VISION
+//     //   }`,
+//     //   req.body
+//     // )
+//     console.log('DATA: ', data)
+//     res.json(data)
+//   } catch (err) {
+//     next(err)
+//   }
+// */
+// })
