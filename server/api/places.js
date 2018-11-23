74a93516443a3ee1e6e40dd48292c5e5ce198b86
jstoobz <stephensja7@gmail.com>
2018-11-23 17:54:30 -0500

    updates with routes

--- server/api/places.js
+++ server/api/places.js
@@ -1,12 +1,12 @@
 const router = require('express').Router()
 const axios = require('axios')
 const { Place } = require('../db/models')
-module.exports = router
-
 const yelpConfig = {
   key: process.env.YELP_BUSINESS_SEARCH
 }
+module.exports = router
 
+//GET --> api/places/recent
 router.get('/recent', async (req, res, next) => {
   try {
     const places = await Place.findAll()
@@ -16,6 +16,7 @@ router.get('/recent', async (req, res, next) => {
   }
 })
 
+//GET --> api/places/nearby/:lat/:lng
 // router.post('/recent', async (req, res, next) => {
 //   const {
 //     name,
@@ -63,9 +64,62 @@ router.get('/nearby/:lat/:lng', async (req, res, next) => {
         }
       }
     )
+    console.log('THIS IS THE YELP DATA', data)
     res.json(data)
   } catch (err) {
     next(err)
+  }
+})
+
+//GET ==> api/places/recent/:id
+router.get('/recent/:id', async (req, res, next) => {
+  try {
+    const { data } = await axios.get(
+      `https://api.yelp.com/v3/businesses/${id}`,
+      {
+        headers: {
+          Authorization: `Bearer ${yelpConfig.key}`
+        }
+      }
+    )
+
+    console.log('YELP DATA for SINGLE BUSINESS', data)
+    res.json(data)
+  } catch (err) {
+    next(err)
+  }
+})
+
+//POST --> api/places/recent
+router.post('/recent', async (req, res, next) => {
+  const userId = 1 //req.user
+  const { name, image_url, id } = req.body
+  const date = new Date()
+  const dateVisited = date.toString()
+
+  try {
+    await Place.findOrCreate({
+      where: {
+        userId,
+        name,
+        imageUrl: image_url,
+        yelpId: id
+      }
+    })
+
+    const [numberOfAffectedRows, affectedRows] = await Place.update(
+      {
+        dateVisited
+      },
+      {
+        where: { yelpId: id, userId },
+        returning: true
+      }
+    )
+
+    res.json(affectedRows)
+  } catch (err) {
+    console.error(err)
   }
 })
 
