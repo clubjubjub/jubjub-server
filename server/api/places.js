837fa5beaf64091519bc5458c0085777ddff356f
Loren Mariquit <l.mariquit@gmail.com>
2018-11-24 23:45:15 -0500

    changed back to harcoded userId. Need to revisit soon

--- server/api/places.js
+++ server/api/places.js
@@ -92,8 +92,8 @@ router.get('/recent/:id', async (req, res, next) => {
 
 //POST --> api/places/recent
 router.post('/recent', async (req, res, next) => {
-  console.log('THE REQ USER: ', req.user)
-  const userId = req.user
+  // console.log('THE REQ USER: ', req.user)
+  const userId = 2 //req.user
   const { name, image_url, id } = req.body
   const date = new Date()
   const dateVisited = date.toString()
