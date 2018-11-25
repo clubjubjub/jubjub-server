08d83004d7d0a2ac1629b9876000434aea8e4659
Loren Mariquit <l.mariquit@gmail.com>
2018-11-25 15:42:04 -0500

    adding a console log

--- server/api/places.js
+++ server/api/places.js
@@ -96,7 +96,7 @@ router.get('/recent/:id', async (req, res, next) => {
 
 //POST --> api/places/recent
 router.post('/recent', async (req, res, next) => {
-  // console.log('THE REQ USER: ', req.user)
+  console.log('THE REQ USER: ', req.user)
   const userId = req.user.userId
   const { name, image_url, id } = req.body
   const date = new Date()
