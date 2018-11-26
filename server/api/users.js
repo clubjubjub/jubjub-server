bfd936c45c63063d3c7507729bf58552d8fc4a3b
Tyler Swartz <tmswartz12@gmail.com>
2018-11-26 13:41:19 -0500

    update route for user email

--- server/api/users.js
+++ server/api/users.js
@@ -12,3 +12,22 @@ router.get('/', async (req, res, next) => {
     next(err)
   }
 })
+
+//update user email
+router.put('/email', async (req, res, next) => {
+  const userId = req.user.id
+  const email = req.body.email
+  try {
+    const user = await User.findOne({
+      where: {
+        userId
+      }
+    })
+    const updated = user.update({
+      email
+    })
+    res.json(updated)
+  } catch (err) {
+    next(err)
+  }
+})
