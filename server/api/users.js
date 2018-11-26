f81af241b18880e5c5c5c018d53fedfdbd7b468f
Tyler Swartz <tmswartz12@gmail.com>
2018-11-26 16:45:52 -0500

    updated to number type

--- server/api/users.js
+++ server/api/users.js
@@ -15,7 +15,8 @@ router.get('/', async (req, res, next) => {
 
 router.get('/:id', async (req, res, next) => {
   console.log('inside user route')
-  const id = req.params.id
+  const id = Number(req.params.id)
+  console.log(id, typeof id)
   try {
     const user = await User.findById(id)
     console.log('this is the user', user)
