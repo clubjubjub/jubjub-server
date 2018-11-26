4cdd9e984389729b10ef67bbc4ab52c197e4aaef
Tyler Swartz <tmswartz12@gmail.com>
2018-11-26 13:47:25 -0500

    hard coded userId to 1 for testing put route

--- server/api/users.js
+++ server/api/users.js
@@ -15,7 +15,7 @@ router.get('/', async (req, res, next) => {
 
 //update user email
 router.put('/email', async (req, res, next) => {
-  const userId = req.user.id
+  const userId = 1
   const email = req.body.email
   try {
     const user = await User.findOne({
