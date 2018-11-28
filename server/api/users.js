3e57b8fe3762bde5a3e4722579b46f28653f1e7f
jstoobz <stephensja7@gmail.com>
2018-11-28 11:29:08 -0500

    updates for user avatar

--- server/api/users.js
+++ server/api/users.js
@@ -58,7 +58,7 @@ router.put('/avatar', async (req, res, next) => {
 
     s3.upload(params, function(err, data) {
       if (err) console.log(`Error: ${err}`)
-      if (data) console.log(`Uploaded in: ${data}`)
+      if (data) console.log(`Uploaded in: ${JSON.stringify(data)}`)
     })
 
     //   const userId = req.user.id
